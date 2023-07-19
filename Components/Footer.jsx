c4b656447a1e7e7cd70bfdcde4c028fb63cdeb3c
Eliam Vazquez <bosc790@gmail.com>
2023-07-19 00:16:17 -0700

    V0.1.1

---
+++ Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () =>{
+    return (
+        <>
+            <footer>
+                Footer
+            </footer>
+        </>
+    )
+}
+
+export default Footer
