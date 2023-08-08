8be8ee25d7d4afe0609f6039f83facc69b082b3c
Eliam Vazquez <bosc790@gmail.com>
2023-08-07 21:28:36 -0700

    V0.1.7

---
+++ Components/Modals/ModalItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModalItem = () => {
+  return (
+    <div>ModalItem</div>
+  )
+}
+
+export default ModalItem
--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -5,30 +5,8 @@ import Logo from '../public/Images/tecnm.png'
 import Image from 'next/image';
 
 const Navbar = () => {
+
   const router = useRouter();
-
-  useEffect(() =>{
-    const headerEl = document.querySelector("#landing-header");
-
-    const observerOptions = {
-      root: null,
-      rootMargin: "0px", // en cuanto se vea el elemento
-      threshold: 0.9, // porcentaje de visibilidad
-    }
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        const { isIntersecting } = entry
-        if (isIntersecting) {
-          const color = entry.target.getAttribute("data-header-color")
-          headerEl.style.color = color
-        }
-      })
-    }, observerOptions)
-
-    const sectionElements = document.querySelectorAll(".landing-section")
-    sectionElements.forEach((section) => observer.observe(section))
-  }, [])
   
   return (
     <>
@@ -41,11 +19,7 @@ const Navbar = () => {
         </div>
         <nav></nav>
       </header>
-      
     </>
-    
-
-    
   )
 }
 
