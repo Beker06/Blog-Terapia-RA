cf44e1921ff2ff44656c666c166751e0fe9c7f48
Eliam Vazquez <bosc790@gmail.com>
2023-08-25 22:39:47 -0700

    V0.3.2

--- Components/Sections/Conclusion/ConclusionDos.jsx
+++ Components/Sections/Conclusion/ConclusionDos.jsx
@@ -7,7 +7,43 @@ const ConclusionDos = () => {
         <div className='flex w-full mt-[8%] justify-center'>
             <div className='flex flex-col text-white w-[70%] text-justify text-lg mx-[10%]'>
                 <p>
-                    
+                  Por último, la VR también puede considerarse una tecnología encarnada por 
+                  su capacidad de engañar a los mecanismos cerebrales que regulan la experiencia 
+                  del cuerpo. Esta capacidad ofrece nuevas formas -de momento sólo parcialmente 
+                  exploradas- de estructurar, aumentar o sustituir la experiencia del cuerpo con 
+                  fines clínicos. Además, ofrece nuevas formas de evaluar el funcionamiento de 
+                  nuestro cerebro, centrándose directamente en los procesos que subyacen a los 
+                  comportamientos del mundo real.
+                </p>
+                <br/>
+                <p>
+                  Todas estas características hacen de la VR una herramienta clínica eficaz. 
+                  Como se expone en este capítulo, y de acuerdo con los resultados de dos 
+                  meta-revisiones recientes, las investigaciones existentes respaldan el uso 
+                  clínico de la VR en la evaluación y el tratamiento de los trastornos de 
+                  ansiedad, el control del dolor y los trastornos alimentarios y de peso, con 
+                  efectos a largo plazo que se generalizan al mundo real. Estudios recientes 
+                  también han proporcionado apoyo preliminar para el uso de la VR en la evaluación 
+                  y el tratamiento de la psicosis, las adicciones y el autismo.
+                </p>
+                <br/>
+                <p>
+                  Un nuevo impulso a la investigación y las aplicaciones clínicas de la VR viene 
+                  dado por la reciente disponibilidad de equipos de VR comerciales y de bajo 
+                  coste. Los teléfonos inteligentes y los vídeos de 360 grados también harán 
+                  posible el desarrollo de una nueva generación de aplicaciones de VR de autoayuda 
+                  que harán más accesible el tratamiento de la salud mental a las personas que 
+                  no tienen tiempo o dinero suficientes para ver a un clínico en persona.
+                </p>
+                <br/>
+                <p>
+                  Sin embargo, la VR, como cualquier otra tecnología, no es más que una herramienta, 
+                  que puede utilizarse para bien o para mal. En este sentido, lo que más interesa 
+                  a la psicología clínica es comprender los fenómenos clínicos y no dejarse llevar 
+                  por la idea de que las tecnologías punteras van a suponer necesariamente soluciones 
+                  para los problemas a los que nos enfrentamos. En otras palabras, tendremos que 
+                  dilucidar qué personas pueden beneficiarse de la VR, de qué manera y en qué momentos 
+                  de la evolución psicopatológica.
                 </p>
             </div>
         </div>
--- Components/Sections/Conclusion/ConclusionUno.jsx
+++ Components/Sections/Conclusion/ConclusionUno.jsx
@@ -8,7 +8,54 @@ const ConclusionUno = () => {
             <div className='flex flex-col text-white w-[70%] text-justify text-lg mx-[10%]'>
                 <h1 className='uppercase text-2xl mb-6'>Conclusión</h1>
                 <p>
-                    
+                  La terapia de realidad virtual (TRV) es una técnica de tratamiento innovadora 
+                  que ha demostrado ser efectiva en diversas áreas de la salud mental, incluyendo 
+                  trastornos emocionales. Este estudio piloto tiene como objetivo investigar la 
+                  efectividad de la TRV en el tratamiento de trastornos emocionales en pacientes 
+                  hispanohablantes. Se reclutaron participantes con diagnóstico de trastornos 
+                  emocionales, se asignaron aleatoriamente a un grupo de intervención  o un grupo 
+                  de control (terapia tradicional) y se midió la evolución de los síntomas 
+                  emocionales a través de diversas herramientas de evaluación antes y después 
+                  del tratamiento. Los resultados preliminares indican que la terapia de realidad 
+                  virtual puede ser una herramienta prometedora en el tratamiento de trastornos 
+                  emocionales en pacientes hispanohablantes, pero se necesitan estudios más 
+                  amplios para confirmar estos hallazgos.
+                </p>
+                <br/>
+                <p>
+                  Desde el punto de vista tecnológico, la realidad virtual (VR) es un conjunto 
+                  de tecnologías de fantasía: un casco, unos rastreadores y un sistema de 
+                  visualización en 3D. Sin embargo, desde el punto de vista psicológico, la 
+                  VR es a la vez una tecnología de simulación, una tecnología cognitiva y una 
+                  tecnología corporal.
+                </p>
+                <br/>
+                <p>
+                  En primer lugar, la VR es una forma de simulación de la realidad. En concreto, 
+                  lo que distingue a la VR de otros medios es la sensación de presencia: la 
+                  sensación de "estar ahí" dentro de la experiencia virtual producida por la 
+                  tecnología. El poder de simulación de la VR la convierte en la herramienta 
+                  perfecta para el aprendizaje experimental. Por un lado, la VR permite aprender 
+                  a través de la reflexión sobre la práctica. Por otro lado, la VR puede 
+                  describirse como un sistema imaginal avanzado, o una forma experiencial de 
+                  imaginería que es tan eficaz como la realidad a la hora de inducir respuestas 
+                  emocionales.
+                </p>
+                <br/>
+                <p>
+                  Además, la VR es también una tecnología cognitiva capaz de reproducir los 
+                  mecanismos que subyacen al funcionamiento del cerebro. Como han sugerido 
+                  recientemente las investigaciones neurocientíficas, nuestros cerebros son 
+                  máquinas de simulación que desarrollan un modelo interno (simulación) del 
+                  cuerpo y el espacio que lo rodea para proporcionar predicciones sobre la 
+                  entrada sensorial esperada y minimizar el número de errores de predicción 
+                  (o "sorpresa"). La VR funciona de forma similar: utiliza la tecnología para 
+                  crear una experiencia virtual que los individuos pueden manipular y explorar 
+                  como si estuvieran en ella. En otras palabras, la tecnología de la VR intenta 
+                  predecir las consecuencias sensoriales de las acciones de los usuarios 
+                  mostrándoles el mismo resultado esperado por nuestro cerebro en el mundo real. 
+                  Desde este punto de vista, cuanto más se parezca el modelo de VR al modelo 
+                  cerebral, más presente se sentirá el individuo en el mundo de la VR.
                 </p>
             </div>
         </div>
